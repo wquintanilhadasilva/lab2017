28bb3d2a834e869b1bb526292964aa2571d0c93c
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2017-05-25 18:12:47 -0300

    controlando o download do módulo com permissão de usuário

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -11,8 +11,8 @@ import { CursosGuard } from './guards/cursos.guard';
 const appRoutes: Routes = [
     {path: '', component: HomeComponent,  canActivate:[AuthGuard]},
     {path: 'login', component: LoginComponent},
-    {path: 'cursos', loadChildren: 'app/cursos/cursos.module#CursosModule', canActivate:[AuthGuard], canActivateChild:[CursosGuard]},
-    {path: 'alunos', loadChildren: 'app/alunos/alunos.module#AlunosModule', canActivate:[AuthGuard]}
+    {path: 'cursos', loadChildren: 'app/cursos/cursos.module#CursosModule', canLoad:[AuthGuard], canActivate:[AuthGuard], canActivateChild:[CursosGuard]},
+    {path: 'alunos', loadChildren: 'app/alunos/alunos.module#AlunosModule', canLoad:[AuthGuard], canActivate:[AuthGuard]}
 ];
 
 @NgModule({
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './../login/auth.service';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanLoad, Router, Route } from "@angular/router";
 import { Observable } from "rxjs/Observable";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   
-  constructor(private authService: AuthService, private router: Router) {}
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+    console.log('Verificando se o usuário tem acesso ao módulo para liberar o download do módulo no browser.')
+    return this.verificarAcesso();
+  }
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-    if(this.authService.usuarioEstaAutenticado()){
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso() {
+    if (this.authService.usuarioEstaAutenticado()) {
       return true;
     }
     this.router.navigate(['/login']);
